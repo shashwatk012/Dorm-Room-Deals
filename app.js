f973408cd5654852e67601034ec978b97a5c78ec
shashwatk012 <shashwatkesharwani.12@gmail.com>
2022-06-18 04:48:54 +0530

    b

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ SignupSchema.methods.tokengenerator = async function () {
   try {
     const token = jwt.sign(
       { _id: this._id.toString() },
-      `${process.env.SECRET_KEY}`
+      "mynameisxyzemailidxyzphonenumberxyz"
     );
     this.tokens = this.tokens.concat({ token: token });
     await this.save();
